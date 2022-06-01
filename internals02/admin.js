5e21cd65cde0bbdf9a2e17583311bc854f509739
Cian <cian.callaghan@student.htw-berlin.de>
2022-06-01 09:45:50 +0200

    2022-06-01

--- internals02/admin.js
+++ internals02/admin.js
@@ -31,6 +31,64 @@ staff_button.onclick = function() {
 		staff_overview.style.display = "flex";
 }
 
+// Filter Students
+function filterDepartment() {
+		let selectedDepartment = document.getElementById("filter_department").value;
+		let studentTable = document.getElementById("student_table");
+		let tr = Array.from(studentTable.getElementsByTagName("tr")).slice(1);
+		if (selectedDepartment == "") {
+				for (var i of tr) {
+						i.style.display = "";
+				}
+		} else {
+				let trs = tr.filter(function(e) {
+						return !e.innerText.includes(selectedDepartment);
+				});
+				for (var i of trs) {
+						i.style.display = "none";
+				}
+		}
+}
+
+function filterSemester() {
+		let selectedSemester = document.getElementById("filter_semester").value;
+		let studentTable = document.getElementById("student_table");
+		let tr = Array.from(studentTable.getElementsByTagName("tr")).slice(1);
+		if (selectedSemester == "") {
+				for (var i of tr) {
+						i.style.display = "";
+				}
+		} else if (selectedSemester == "Winter") {
+				for (var i of tr) {
+						i.style.display = "";
+				}
+				let winter = [10, 11, 01, 02, 03];
+				let trs = tr.filter(function(e) {
+						var month = new Date(Date.parse(e.innerText.split("\t")[7])).getMonth() + 1;
+						if (!winter.includes(month)) {
+								return e;
+						}
+				});
+				for (var i of trs) {
+						i.style.display = "none";
+				}
+		} else if (selectedSemester == "Summer") {
+				for (var i of tr) {
+						i.style.display = "";
+				}
+				let summer = [04, 05, 06, 07, 08, 09];
+				let trs = tr.filter(function(e) {
+						var month = new Date(Date.parse(e.innerText.split("\t")[7])).getMonth() + 1;
+						if (!summer.includes(month)) {
+								return e;
+						}
+				});
+				for (var i of trs) {
+						i.style.display = "none";
+				}
+		}
+}
+
 // Add Student Form
 add_student_button.onclick = function() {
 		hide_all();
@@ -39,26 +97,30 @@ add_student_button.onclick = function() {
 
 function submitAddStudentForm(event) {
 		event.preventDefault();
-		var student_id = add_student.student_id.value;
-		var student_name = add_student.name.value;
-		var student_surname = add_student.surname.value;
-		var student_dob = add_student.dob.value;
-		var student_gender = add_student.gender.value;
-		var student_department = add_student.department.value;
-		var student_email = add_student.email.value;
 
-		var tr = document.createElement("tr");
-		tr.innerHTML = "<td>"+student_id+"</td>"
-				+"<td>"+student_name+"</td>"
-				+"<td>"+student_surname+"</td>"
-				+"<td>"+student_dob.toString()+"</td>"
-				+"<td>"+student_gender+"</td>"
-				+"<td>"+student_department+"</td>"
-				+"<td>"+student_email.toString()+"</td>"
-		student_table.appendChild(tr);
-		
-		hide_all();
-		student_overview.style.display = "flex";
+		// Check DOB
+		var currentDate = new Date();
+		currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+		var min17 = new Date();
+		min17.setFullYear(min17.getFullYear() - 17);
+		var max60 = new Date();
+		max60.setFullYear(max60.getFullYear() - 60);
+		var student_dob = new Date(Date.parse(add_student.dob.value));
+		if (student_dob.valueOf() > currentDate.valueOf()) {
+				alert("Invalid DOB");
+		}
+		else if (student_dob.valueOf() > min17.valueOf()) {
+				alert("Invalid DOB");
+		} else if  (student_dob.valueOf() < max60.valueOf()) {
+				alert("Invalid DOB");
+		}
+
+		// Check Joining Date
+		var earliestJoin = new Date(2015, 0);
+		var joiningDate = new Date(add_student.joining_date.value);
+		if (joiningDate < earliestJoin) {
+				alert("Invalid Joining Date");
+		}
 }
 
 cancel_add_student_form.onclick = function() {
@@ -73,7 +135,8 @@ add_staff_button.onclick = function() {
 		add_staff_form.style.display = "flex";
 }
 
-submit_add_staff_form.onclick = function() {
+function submitAddStaffForm(event) {
+		event.preventDefault();
 }
 
 cancel_add_staff_form.onclick = function() {
