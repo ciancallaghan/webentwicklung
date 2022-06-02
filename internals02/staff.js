2e29a3275add7c8983c00fb2ac7fcad48defb7f0
Cian <cian.callaghan@student.htw-berlin.de>
2022-06-02 12:39:23 +0200

    2022-06-02

--- internals02/staff.js
+++ internals02/staff.js
@@ -46,3 +46,61 @@ computer_networks_button.onclick = function(){
 		hide_all();
 		computer_networks_students.style.display = "flex";
 }
+
+// Filter Students
+function filterDepartment() {
+		let selectedDepartment = document.getElementById("filter_department").value;
+		let studentTable = document.getElementById("student_table");
+		let tr = Array.from(studentTable.getElementsByTagName("tr")).slice(1);
+		if (selectedDepartment == "") {
+				for (var i of tr) {
+						i.style.display = "";
+				}
+		} else {
+				let trs = tr.filter(function(e) {
+						return !e.innerText.includes(selectedDepartment);
+				});
+				for (var i of trs) {
+						i.style.display = "none";
+				}
+		}
+}
+
+function filterSemester() {
+		let selectedSemester = document.getElementById("filter_semester").value;
+		let studentTable = document.getElementById("student_table");
+		let tr = Array.from(studentTable.getElementsByTagName("tr")).slice(1);
+		if (selectedSemester == "") {
+				for (var i of tr) {
+						i.style.display = "";
+				}
+		} else if (selectedSemester == "Winter") {
+				for (var i of tr) {
+						i.style.display = "";
+				}
+				let winter = [10, 11, 01, 02, 03];
+				let trs = tr.filter(function(e) {
+						var month = new Date(Date.parse(e.innerText.split("\t")[7])).getMonth() + 1;
+						if (!winter.includes(month)) {
+								return e;
+						}
+				});
+				for (var i of trs) {
+						i.style.display = "none";
+				}
+		} else if (selectedSemester == "Summer") {
+				for (var i of tr) {
+						i.style.display = "";
+				}
+				let summer = [04, 05, 06, 07, 08, 09];
+				let trs = tr.filter(function(e) {
+						var month = new Date(Date.parse(e.innerText.split("\t")[7])).getMonth() + 1;
+						if (!summer.includes(month)) {
+								return e;
+						}
+				});
+				for (var i of trs) {
+						i.style.display = "none";
+				}
+		}
+}
